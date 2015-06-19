bb37262a5a64928c041d2101f06e93897285059b
Rose Anne Concepcion <roseanne.concepcion@gmail.com>
2015-06-19 16:24:58 +0800

    adds video modal

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+//sticky nav
 $(window).scroll(function(){
   if($(window).scrollTop() > 100) {
     $(".default-navbar").addClass("-fixed");    
@@ -5,3 +6,26 @@ $(window).scroll(function(){
     $(".default-navbar").removeClass("-fixed");    
   }
 });
+
+//centered modal
+$(function(){
+  function reposition() {
+    var modal = $(this),
+        dialog = modal.find('.modal-dialog');
+    modal.css('display', 'block');
+
+    dialog.css("margin-top", Math.max(0, ($(window).height() - dialog.height()) / 2));
+  }
+  $('.modal').on('show.bs.modal', reposition);
+  $(window).on('resize', function(){
+    $('.modal:visible').each(reposition);
+  });
+});
+
+//stop modal when closed
+$(function(){
+  $(".modal").on("hidden.bs.modal", function(){
+    $('.modal iframe').attr("src", $(".modal iframe").attr("src"))
+  });
+})
+
