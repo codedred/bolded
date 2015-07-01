4d7d49a554b78f1c98882a3446e9c8667ee9233b
Rose Anne Concepcion <roseanne.concepcion@gmail.com>
2015-07-01 16:20:11 +0800

    fixes events font styling, our objectives section and invites section styling

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,3 @@
-//sticky nav
-$(window).scroll(function(){
-  if($(window).scrollTop() > 100) {
-    $(".default-navbar").addClass("-fixed");    
-    $(".logo-white").css("display", "none");    
-    $(".logo").css("display", "block");    
-  } else {
-    $(".default-navbar").removeClass("-fixed");    
-    $(".logo").css("display", "none");    
-    $(".logo-white").css("display", "block");    
-  }
-});
-
 //centered modal
 $(function(){
   function reposition() {
